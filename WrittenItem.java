c20897653d6e958b86b764b395b86c80b411e5f3
KishoreVS <33390009+KishoreVS@users.noreply.github.com>
2019-07-16 16:02:31 +0530

    Update WrittenItem.java

--- WrittenItem.java
+++ WrittenItem.java
@@ -1,31 +1,20 @@
 package lab2;
 
-public class WrittenItem extends Item{
-	WrittenItem(int idNum,int noOfCopies,String title)
-	{
-		super(idNum,noOfCopies,title);
-	}
-	
-	public WrittenItem()
-	{
-		
-	}
-	
-	private String author;
-	
-	public String getAuthor()
-	{
-		return author;
-	}
-	
-	public void setAuthor(String author)
-	{
-		this.author=author;
-	}
-	
-	public String toString()
-	{
-		return "WrittenItem [author= "+author+", getAuthor= "+getAuthor()+", toString()= "+super.toString()+", getIdNum()= "+getIdNum()+", getNoOfCopies()= "+getNoOfCopies()+", getTitle()= "+getTitle()+", getClass()= "+getClass()+", hashCode()= "+hashCode()+" ]";
-	}
+public class WrittenItem extends Item 
+{
+private String author;
 
+public String getAuthor() 
+{
+	return author;
+}
+
+public void setAuthor(String author)
+{
+	this.author = author;
+}
+public WrittenItem() 
+{
+	System.out.println("This is a Written Item");
+}
 }
